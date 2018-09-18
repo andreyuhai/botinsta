a5729a37aaf219693aa4b5735add58b685b07481
andreyuhai <yuhai.ndre@gmail.com>
2018-09-19 00:14:05 +0300

    require -> require_relative

--- lib/botinsta/class_methods.rb
+++ lib/botinsta/class_methods.rb
@@ -1,9 +1,13 @@
-# require 'actions'
-require 'helpers'
-require 'login'
-require 'modes'
-require 'pages'
-require 'requests'
+require_relative 'actions'
+require_relative 'data/media_data'
+require_relative 'data/user_data'
+require_relative 'data/page_data'
+require_relative 'helpers'
+require_relative 'login'
+require_relative 'modes'
+require_relative 'pages'
+require_relative 'requests'
+
 
 module ClassMethods
 
