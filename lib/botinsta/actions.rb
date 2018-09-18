5696359340bc1b2fab202bf921cff10eea3407aa
andreyuhai <yuhai.ndre@gmail.com>
2018-09-19 01:57:36 +0300

    Function descriptions added for documentation.

--- lib/botinsta/actions.rb
+++ lib/botinsta/actions.rb
@@ -1,7 +1,10 @@
 # Actions you can do on Instagram
 module Actions
 
-  # Simple actions
+  # Likes media given by media id.
+  #
+  # @param media_id [String]
+  # @return [true, false] returns true on success, false otherwise.
   def like_media(media_id)
     url_like = "https://www.instagram.com/web/likes/#{media_id}/like/"
     print_try_message(action: :like, data: media_id)
@@ -15,6 +18,10 @@ module Actions
     response.code == '200' && response_data['status'] == 'ok' ? true : false
   end
 
+  # Unikes media given by media id.
+  #
+  # @param media_id [String]
+  # @return [true, false] returns true on success, false otherwise.
   def unlike_media(media_id)
     url_unlike = "https://www.instagram.com/web/likes/#{media_id}/unlike/"
     print_try_message(action: :unlike, data: media_id)
@@ -28,6 +35,10 @@ module Actions
     response.code == '200' && response_data['status'] == 'ok' ? true : false
   end
 
+  # Follows user given by user_id.
+  #
+  # @param user_id [String]
+  # @return [true, false] returns true on success, false otherwise.
   def follow_user(user_id)
     url_follow = "https://www.instagram.com/web/friendships/#{user_id}/follow/"
     print_try_message(action: :follow, data: user_id)
@@ -41,6 +52,10 @@ module Actions
     response.code == '200' && response_data['result'] == 'following' ? true : false
   end
 
+  # Unfollows user given by user_id.
+  #
+  # @param user_id [String]
+  # @return [true, false] returns true on success, false otherwise.
   def unfollow_user(user_id)
     url_unfollow = "https://www.instagram.com/web/friendships/#{user_id}/unfollow/"
     print_try_message(action: :unfollow, data: user_id)
@@ -54,7 +69,10 @@ module Actions
     response.code == '200' && response_data['status'] == 'ok' ? true : false
   end
 
-  # More complicated actions
+  # Likes the media if it doesn't exist in the database.
+  #
+  # @param media [MediaData]
+  # @return [true,false] returns true on success, false otherwise.
   def like_if_not_in_db(media)
     return false if media.exists_in_db?(@table_likes)
 
@@ -69,6 +87,10 @@ module Actions
     end
   end
 
+  # Follows the user if it doesn't exist in the database.
+  #
+  # @param user [MediaData]
+  # @return [true,false] returns true on success, false otherwise.
   def follow_if_not_in_db(user)
     return false if user.exists_in_db?(@table_follows)
 
