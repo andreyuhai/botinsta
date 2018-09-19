88d6cd4f959be82d8313b3f9028e1b29a21d4df2
andreyuhai <yuhai.ndre@gmail.com>
2018-09-19 18:15:50 +0300

    Comments added for documentation.

--- lib/botinsta/helpers.rb
+++ lib/botinsta/helpers.rb
@@ -4,12 +4,12 @@ module Helpers
 
   # Prints out the current time
   # @example
-  #   print_time_stamp #=> "2018-09-19 12:14:43"
+  #   print_time_stamp # => "2018-09-19 12:14:43"
   def print_time_stamp
     print "#{Time.now.strftime('%Y-%m-%d %H:%M:%S')}\t"
   end
 
-  # Prints success messages depending on the action.
+  # Prints success message for a specified action.
   #
   # @option params [Symbol] :action The action performed.
   # @option params [String] :data The id that the action is performed on.
@@ -27,6 +27,10 @@ module Helpers
     puts success_string
   end
 
+  # Prints error message for a specified action.
+  #
+  # @option params [Symbol] :action The action performed.
+  # @option params [String] :data The id that the action is performed on.
   def print_error_message(**params)
     action = case params[:action]
              when :like     then 'like media'
@@ -41,6 +45,10 @@ module Helpers
 
   end
 
+  # Prints login status message for the account
+  #
+  # @option params [Symbol] :result Login status
+  # @option params [String] :username
   def print_login_message(**params)
     result = case params[:result]
              when :success  then 'Successfully logged in as '
@@ -50,6 +58,11 @@ module Helpers
     puts result.colorize(:red) + params[:username]
   end
 
+  # Prints messages when trying to take some action.
+  #
+  # @option params [Symbol] :action The action that the bot is trying to take.
+  # @option params [String, Integer] :data The data which will be
+  #   affected by the specified action.
   def print_try_message(**params)
     action = case params[:action]
              when :login    then 'Trying to login ...'
@@ -64,15 +77,21 @@ module Helpers
     sleep(1)
   end
 
+  # Prints action sum. Used before logging out.
+  #
+  # @example print_action_sum usage
+  #   bot.print_action_sum # => 2018-09-19 17:41:11	Liked: 0 Followed: 0 Unfollowed: 0
   def print_action_sum
     string =  'Liked: ' + @total_likes.to_s.colorize(:red) +
               ' Followed: ' + @total_follows.to_s.colorize(:red) +
               ' Unfollowed: ' + @total_unfollows.to_s.colorize(:red)
-    # TODO: Comments!
     print_time_stamp
     puts string
   end
 
+  # Prints out when the current is set to some specified tag.
+  #
+  # @param tag [String] current tag.
   def print_tag_message(tag)
     print_time_stamp
     puts 'Current tag is set to '.colorize(:blue) + '#' + tag
@@ -87,8 +106,7 @@ module Helpers
   end
 
   # Handles the creation and connection of a database and its tables.
-  #
-  # 
+  # Sets @table_follows and @table_likes to the related tables for further use.
   def handle_database_creation
     database = Sequel.sqlite('./actions_db.db') # memory database, requires sqlite3
     database.create_table? :"#{@username}_follows" do
@@ -108,6 +126,9 @@ module Helpers
     @table_likes    = database[:"#{@username}_likes"]
   end
 
+  # Reassigns the database related variables to the first entry
+  #   in the database.
+  # Used after a deletion from the database.
   def refresh_db_related
     return if @table_follows.empty?
 
@@ -115,15 +136,19 @@ module Helpers
     @last_follow_time = @first_db_entry[:follow_time]
   end
 
+  # The method that is used to delete a user from the database
+  #   after unfollowing the user.
+  # @todo This method needs to be replaced with a better and DRY one.
+  # @param user_id [String, Integer] id of the user to be unfollowed.
   def delete_from_db(user_id)
-    # TODO: Think of a better way, this is not DRY
     @table_follows.where(user_id: user_id).delete
   end
 
   # Calculates if a day is past since the first follow entry in the database.
   #
-  # @param last_time [Time] a Time instance.
-  # @return [true, false] true if a day is past since the first follow entry in the database, false otherwise.
+  # @param last_time [Time] a Time instance, used with @last_follow_time.
+  # @return [Boolean] true if a day is past since
+  #   the first follow entry in the database, false otherwise.
   def one_day_past?(last_time)
     ((Time.now - last_time) / 86_400) >= 1
   end
