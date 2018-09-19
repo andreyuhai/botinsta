97520921b577a3e38ac3d6d55a4313f883340908
andreyuhai <yuhai.ndre@gmail.com>
2018-09-19 22:23:56 +0300

    Specs changed.

--- spec/botinsta_spec.rb
+++ spec/botinsta_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Botinsta do
   it "has a version number" do
     expect(Botinsta::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
