ec63f5c21a994402593bde8ea1c55a284ae12ee2
andreyuhai <yuhai.ndre@gmail.com>
2018-09-20 22:30:57 +0300

    New parameter unfollow_threshold added to the example.

--- example/example.rb
+++ example/example.rb
@@ -3,10 +3,15 @@ require 'botinsta'
 bot = Botinsta.new ({ username: 'YOUR_USERNAME',
                       password: 'YOUR_PASSWORD',
                       tags:              ['photography','vsco','b&w'],
-                      likes_per_tag:     60,
                       tag_blacklist:     ['nsfw','sexy','hot'],
+                      likes_per_tag:     60,
                       unfollows_per_run: 200,
-                      follows_per_tag:   10
+                      follows_per_tag:   10,
+                      unfollow_threshold:  { seconds: 0,
+                                             minutes: 0,
+                                             hours:   1,
+                                             days:    0
+                      }
 })
 
 bot.start
