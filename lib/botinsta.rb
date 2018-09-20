89291281c4387573f89878e2677d6eb72fceb0b1
andreyuhai <yuhai.ndre@gmail.com>
2018-09-20 21:49:07 +0300

    Unfollow threshold parameter added.

--- lib/botinsta.rb
+++ lib/botinsta.rb
@@ -5,6 +5,8 @@ require 'mechanize'
 require 'sequel'
 require 'sqlite3'
 require 'nokogiri'
+require 'rb-readline'
+require 'pry'
 
 require_relative 'botinsta/class_methods'
 
@@ -14,26 +16,32 @@ class Botinsta
 
   include ClassMethods
 
-  DEFAULT_PARAMETERS = { tags:              %w[photography fotografia vsco],
-                         tag_blacklist:     %w[nsfw hot sexy],
-                         user_blacklist:    [],
-                         likes_per_tag:     10,
-                         unfollows_per_run: 200,
-                         follows_per_tag:   50
+  DEFAULT_PARAMETERS = { tags:                %w[photography fotografia vsco],
+                         tag_blacklist:       %w[nsfw hot sexy],
+                         user_blacklist:      [],
+                         likes_per_tag:       10,
+                         follows_per_tag:     50,
+                         unfollows_per_run:   200,
+                         unfollow_threshold:  { seconds: 0,
+                                                minutes: 0,
+                                                hours:   1,
+                                                days:    0
+                         }
   }.freeze
 
   def initialize(**params)
 
     params = DEFAULT_PARAMETERS.merge(params)
 
-    @username           = params[:username]
-    @password           = params[:password]
-    @tags               = params[:tags]
-    @tag_blacklist      = params[:tag_blacklist]
-    @user_blacklist     = params[:user_blacklist]
-    @likes_per_tag      = params[:likes_per_tag]
-    @follows_per_tag    = params[:follows_per_tag]
-    @unfollows_per_run  = params[:unfollows_per_run]
+    @username             = params[:username]
+    @password             = params[:password]
+    @tags                 = params[:tags]
+    @tag_blacklist        = params[:tag_blacklist]
+    @user_blacklist       = params[:user_blacklist]
+    @likes_per_tag        = params[:likes_per_tag]
+    @follows_per_tag      = params[:follows_per_tag]
+    @unfollows_per_run    = params[:unfollows_per_run]
+    @unfollow_threshold   = params[:unfollow_threshold]
 
     @total_likes        = 0
     @total_follows      = 0
