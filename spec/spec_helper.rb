dcf506f7d7f762115e69b83d9b06d0ab06c60d1b
andreyuhai <yuhai.ndre@gmail.com>
2018-09-21 00:15:28 +0300

    Coveralls added.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,9 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 require 'bundler/setup'
 require 'botinsta'
+require 'coveralls'
+
+Coveralls.wear!
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
